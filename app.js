9786f3d65ff736dcd3c07e7e6078a02c260f90d6
Muditha256 <muditharandeep123@gmail.com>
2024-06-27 16:07:41 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,137 @@
+//Arithmetic operations
+// let sum = 10+20;
+
+// + | - | * | / | %    
+
+// console.log(sum);
+
+//------------------------------
+
+// Assigment operations
+
+// = | += | -= | *= | /=
+
+//------------------------------
+
+// Comparison operations
+
+// == | != | >= | <= | >= | > | < 
+
+// ===
+
+// let isEqual = (5 == '5');
+
+// let isThisEqual = (5 === '5');
+
+// console.log(isEqual);
+
+// console.log(isThisEqual);
+
+//------------------------------
+// Logical operations
+
+// && | || | !
+
+
+// const names = "saman";
+
+// names = "kamal";
+
+// console.log(names);
+
+
+
+// {
+
+//     let age = 12;
+
+// }
+
+// console.log(age);
+
+// class Person {
+//     Person(){
+
+//     }
+
+//     public void setName(){
+
+//     }
+// }
+
+// let address = "gamapaha";
+
+// function sample() {
+//     var age = 12;
+//     //let name = "saman";
+//     var name = "kamal";
+// }
+
+// age = 20;
+
+// console.log(age);
+
+// console.log( name);
+
+// var age = 50;
+
+// console.log(age);
+
+// let age = 30
+
+// console.log(age)
+
+
+// const customer = {
+//      name: "saman" ,
+//      age: 20 ,
+//      address: "gamapaha" , 
+//      phone: "0712345678" , 
+//      email: "saman.com"
+// }
+
+// console.log(name);
+
+// console.log(customer.name);
+// console.log(customer.age);
+// console.log(customer.address);
+
+// const numbers = [1,2,3,4,5,6,7,8,9];
+
+
+
+// for(let i = 0; i < numbers.length; i++){
+//     console.log(numbers[i]);
+// }
+
+// const customers = [
+//     {
+//         name: "saman" ,
+//         age: 20 ,
+//         address: "gamapaha" , 
+//         phone: "0712345678" , 
+//         email: "saman.com"
+//     },
+//     {
+//         name: "kamal" ,
+//         age: 20 ,
+//         address: "gamapaha" , 
+//         phone: "0712345678" , 
+//         email: "kamal.com"
+//     },
+//     {
+//         name: "kamal" ,
+//         age: 20 ,
+//         address: "gamapaha" , 
+//         phone: "0712345678" , 
+//         email: "kamal.com"
+//     }
+// ];
+
+
+
+// console.log(customers[1].name);
+
+// for(let i = 0; i < customers.length; i++){
+//     console.log(customers[i].name);
+// }
